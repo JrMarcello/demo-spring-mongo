041ad12edcf9bbbe20a526761ee40057dbaa846c
Marcello Jr <marcello.dudk@gmail.com>
2019-03-11 22:41:32 -0300

    Criando modulo class

---
+++ src/main/java/br/com/dynamus/api/bsclass/ClassRepository.java
@@ -0,0 +1,10 @@
+package br.com.dynamus.api.bsclass;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "classes", path = "class")
+public interface ClassRepository extends MongoRepository<ClassEntity, String> {
+    //MemberEntity findById(ObjectId _id);
+    //MemberEntity findByName(String cpf);
+}
